cc31bbe3b18cbd93611d7060c40b620f9cd547af
Carlos Fernando Alvarado Lima <carlos.alvarado@alumnos.uneatlantico.es>
2026-03-07 17:33:19 +0100

    Fix: formato de métodos y posicionamiento.
    - Se reorganizaron los métodos para reducir al mínimo el tiempo de vida de las variables globales, y para aplicar proximidad entre métodos siguiendo el formato de "leer de arriba hacia abajo"

--- entregas/alvaradoCarlos/src/Monitora.java
+++ entregas/alvaradoCarlos/src/Monitora.java
@@ -16,7 +16,7 @@ public class Monitora {
         for (int i = 0; i < niños.length; i++) {
             if (niños[i] == null) {
                 niños[i] = niño;
-                System.out.println("Llega " + niño.nombre() + " " + "(" + niño.edad() + " años" + ")");
+                System.out.println("Llega " + niño.nombre() + " (" + niño.edad() + " años)");
                 System.out.println(niño.nombre() + " pasa a la cola de " + nombre);
                 cantidadDeNiños++;
                 return;
@@ -26,14 +26,11 @@ public class Monitora {
     }
 
     public Niño[] entregaNiños(Monitora monitora) {
-        if (!haySuficientesNinos())
-            return new Niño[0];
+        if (!haySuficientesNinos()) return new Niño[0];
 
         Niño[] grupoAEnviar = extraerGrupo(cantidadDeNiños);
-
         Niño[] grupoRecibido = monitora.recibeNiñosParaJugar(grupoAEnviar);
 
-        System.out.println(grupoRecibido.length);
         reacomodarNiños(grupoRecibido.length);
         cantidadDeNiños -= grupoRecibido.length;
 
@@ -41,149 +38,6 @@ public class Monitora {
         mostrarGrupo(grupoRecibido);
 
         return grupoRecibido;
-    }
-
-    private Niño[] extraerGrupo(int tamaño) {
-        Niño[] grupo = new Niño[tamaño];
-        for (int i = 0; i < tamaño; i++) {
-            grupo[i] = niños[i];
-        }
-        return grupo;
-    }
-
-    private boolean haySuficientesNinos() {
-        if (cantidadDeNiños < 5) {
-            System.out.println("No hay suficientes niños para iniciar el juego.");
-            System.out.println("Se necesitan al menos 5 niños.\n");
-            return false;
-        }
-        return true;
-    }
-
-    public String nombre() {
-        return nombre;
-    }
-
-    public int cantidadDeNiños() {
-        return cantidadDeNiños;
-    }
-
-    private void mostrarGrupo(Niño[] grupo) {
-        System.out.println("Niños transferidos:");
-        for (Niño niño : grupo) {
-            if (niño != null) {
-                System.out.println("- " + niño.nombre());
-            } else {
-                System.out.println("niño nulo");
-            }
-        }
-        System.out.println();
-    }
-
-    public Niño[] recibeNiñosParaJugar(Niño[] grupo) {
-        int recibidos = 0;
-        Niño[] grupoRecibido = new Niño[grupo.length];
-
-        for (int i = 0; i < grupo.length; i++) {
-            if (grupo[i] != null) {
-                niños[cantidadDeNiños++] = grupo[i];
-                grupoRecibido[recibidos++] = grupo[i];
-                System.out.println(nombre + " recibió al niño " + grupo[i].nombre());
-            }
-        }
-
-        return grupoRecibido;
-    }
-
-    private void reacomodarNiños(int tamaño) {
-        for (int i = tamaño; i < cantidadDeNiños; i++) {
-            niños[i - tamaño] = niños[i];
-        }
-        for (int i = cantidadDeNiños - tamaño; i < cantidadDeNiños; i++) {
-            niños[i] = null;
-        }
-    }
-
-    public void presentarse() {
-        System.out.println(nombre + ": Hola, soy " + nombre + ", monitora de esta ludoteca");
-    }
-
-    public void prepararCadenaPresentaciones() {
-        for (int i = 0; i < cantidadDeNiños - 1; i++) {
-            niños[i].siguienteNiño(niños[i + 1]);
-        }
-        if (cantidadDeNiños > 0) {
-            niños[cantidadDeNiños - 1].siguienteNiño(null);
-        }
-    }
-
-    public void pideAlPrimerNiñoPresentarse() {
-        if (cantidadDeNiños == 0) {
-            System.out.println("No hay niños en la cola de " + nombre + " para presentarse.");
-            return;
-        }
-
-        niños[0].presentarse();
-    }
-
-    public void pedirCincoPrimerosPresentarse() {
-        int limite = Math.min(5, cantidadDeNiños);
-
-        for (int i = 0; i < limite; i++) {
-            niños[i].presentarse();
-        }
-    }
-
-    public void pedirCincoUltimosPresentarse() {
-        int inicio = Math.max(0, cantidadDeNiños - 5);
-
-        for (int i = inicio; i < cantidadDeNiños; i++) {
-            niños[i].presentarse();
-        }
-    }
-
-    public void pedirNiñosMayoresDe5Presentarse() {
-        boolean hayNiños = false;
-
-        for (int i = 0; i < cantidadDeNiños; i++) {
-            Niño niño = niños[i];
-            if (niño.edad() > 5) {
-                niño.presentarse();
-                hayNiños = true;
-            }
-        }
-
-        if (!hayNiños) {
-            System.out.println("No hay niños mayores de 5 años en la cola.\n");
-        }
-    }
-
-    public void pedirNiñosPorLetra(char letra) {
-        boolean hayNiños = false;
-
-        for (int i = 0; i < cantidadDeNiños; i++) {
-            Niño niño = niños[i];
-            if (niño.nombre().toUpperCase().charAt(0) == Character.toUpperCase(letra)) {
-                niño.presentarse();
-                hayNiños = true;
-            }
-        }
-
-        if (!hayNiños) {
-            System.out.println("No hay niños cuyos nombres empiecen con '" + letra + "'.\n");
-        }
-    }
-
-    public double decirEdadPromedio() {
-        if (cantidadDeNiños == 0) {
-            return 0.0;
-        }
-
-        int suma = 0;
-        for (int i = 0; i < cantidadDeNiños; i++) {
-            suma += niños[i].edad();
-        }
-        return (double) suma / cantidadDeNiños;
     }
 
     public Niño[] separarNiñosMenoresDe5(Monitora dalsy) {
@@ -198,12 +52,8 @@ public class Monitora {
         int idxMayores = 0;
 
         for (int i = 0; i < cantidadDeNiños; i++) {
-            Niño niño = niños[i];
-            if (niño.edad() < 5) {
-                menoresDe5[idxMenores++] = niño;
-            } else {
-                mayoresOIgual5[idxMayores++] = niño;
-            }
+            if (niños[i].edad() < 5) menoresDe5[idxMenores++] = niños[i];
+            else mayoresOIgual5[idxMayores++] = niños[i];
         }
 
         System.out.println("Niños menores de 5 años pasan a " + dalsy.nombre() + ":");
@@ -211,67 +61,140 @@ public class Monitora {
             dalsy.recibeNiño(menoresDe5[i]);
             System.out.println("- " + menoresDe5[i].nombre() + " (" + menoresDe5[i].edad() + " años)");
         }
-        System.out.println();
 
         reorganizarCola(mayoresOIgual5, idxMayores);
-
         System.out.println("Niños que se quedan con " + nombre + " para jugar:");
-        for (int i = 0; i < cantidadDeNiños; i++) {
-            System.out.println("- " + niños[i].nombre() + " (" + niños[i].edad() + " años)");
-        }
-        System.out.println();
+        mostrarGrupo(extraerGrupo(cantidadDeNiños));
 
-        return menoresDe5;
-    }
-
-    public void reorganizarCola(Niño[] nuevosNiños, int cantidad) {
-        for (int i = 0; i < cantidadDeNiños; i++) {
-            niños[i] = null;
-        }
-        cantidadDeNiños = 0;
-
-        for (int i = 0; i < cantidad; i++) {
-            if (nuevosNiños[i] != null) {
-                niños[cantidadDeNiños++] = nuevosNiños[i];
-            }
-        }
+        return Arrays.copyOf(menoresDe5, idxMenores);
     }
 
     public Niño[] entregaTodosNiñosA(Monitora otraMonitora) {
-        if (cantidadDeNiños == 0)
-            return new Niño[0];
+        if (cantidadDeNiños == 0) return new Niño[0];
 
-        Niño[] grupo = Arrays.copyOf(niños, cantidadDeNiños);
-
+        Niño[] grupo = extraerGrupo(cantidadDeNiños);
         for (int i = 0; i < cantidadDeNiños; i++) {
             otraMonitora.recibeNiño(niños[i]);
         }
 
-        for (int i = 0; i < cantidadDeNiños; i++) {
-            niños[i] = null;
-        }
-        int transferidos = cantidadDeNiños;
+        Arrays.fill(niños, null);
         cantidadDeNiños = 0;
 
-        System.out.println(nombre + " transfiere TODOS sus niños a " + otraMonitora.nombre + " INMEDIATAMENTE");
+        System.out.println(nombre + " transfiere TODOS sus niños a " + otraMonitora.nombre() + " INMEDIATAMENTE");
+        return grupo;
+    }
 
-        return Arrays.copyOf(grupo, transferidos);
+    public void presentarse() {
+        System.out.println(nombre + ": Hola, soy " + nombre + ", monitora de esta ludoteca");
+    }
+
+    public void prepararCadenaPresentaciones() {
+        for (int i = 0; i < cantidadDeNiños; i++) {
+            niños[i].siguienteNiño(i < cantidadDeNiños - 1 ? niños[i + 1] : null);
+        }
+    }
+
+    public void pideAlPrimerNiñoPresentarse() {
+        if (cantidadDeNiños > 0) niños[0].presentarse();
+        else System.out.println("No hay niños en la cola de " + nombre);
+    }
+
+    public void pedirCincoPrimerosPresentarse() {
+        int limite = Math.min(5, cantidadDeNiños);
+        for (int i = 0; i < limite; i++) niños[i].presentarse();
+    }
+
+    public void pedirCincoUltimosPresentarse() {
+        int inicio = Math.max(0, cantidadDeNiños - 5);
+        for (int i = inicio; i < cantidadDeNiños; i++) niños[i].presentarse();
+    }
+
+    public void pedirNiñosMayoresDe5Presentarse() {
+        boolean hayNiños = false;
+        for (int i = 0; i < cantidadDeNiños; i++) {
+            if (niños[i].edad() > 5) {
+                niños[i].presentarse();
+                hayNiños = true;
+            }
+        }
+        if (!hayNiños) System.out.println("No hay niños mayores de 5 años en la cola.\n");
+    }
+
+    public void pedirNiñosPorLetra(char letra) {
+        boolean hayNiños = false;
+        for (int i = 0; i < cantidadDeNiños; i++) {
+            if (niños[i].nombre().toUpperCase().startsWith(String.valueOf(letra).toUpperCase())) {
+                niños[i].presentarse();
+                hayNiños = true;
+            }
+        }
+        if (!hayNiños) System.out.println("No hay niños cuyos nombres empiecen con '" + letra + "'.\n");
+    }
+
+    public double decirEdadPromedio() {
+        if (cantidadDeNiños == 0) return 0.0;
+        int suma = 0;
+        for (int i = 0; i < cantidadDeNiños; i++) suma += niños[i].edad();
+        return (double) suma / cantidadDeNiños;
     }
 
     public void mostrarEstado() {
         System.out.println(nombre.toUpperCase() + ":");
-
-        if (cantidadDeNiños == 0) {
-            System.out.println("  Cola vacía\n");
-            return;
+        if (cantidadDeNiños == 0) System.out.println("  Cola vacía\n");
+        else {
+            System.out.println("  Niños en cola: " + cantidadDeNiños);
+            mostrarGrupo(extraerGrupo(cantidadDeNiños));
         }
+    }
 
-        System.out.println("  Niños en cola: " + cantidadDeNiños);
-        for (int i = 0; i < cantidadDeNiños; i++) {
-            Niño niño = niños[i];
-            System.out.println("  - " + niño.nombre() + " (" + niño.edad() + " años)");
+    public Niño[] recibeNiñosParaJugar(Niño[] grupo) {
+        int recibidos = 0;
+        Niño[] grupoRecibido = new Niño[grupo.length];
+        for (Niño n : grupo) {
+            if (n != null && cantidadDeNiños < niños.length) {
+                niños[cantidadDeNiños++] = n;
+                grupoRecibido[recibidos++] = n;
+                System.out.println(nombre + " recibió al niño " + n.nombre());
+            }
+        }
+        return Arrays.copyOf(grupoRecibido, recibidos);
+    }
+
+    public void reorganizarCola(Niño[] nuevosNiños, int cantidad) {
+        Arrays.fill(niños, null);
+        System.arraycopy(nuevosNiños, 0, niños, 0, cantidad);
+        cantidadDeNiños = cantidad;
+    }
+
+    private void reacomodarNiños(int tamaño) {
+        for (int i = tamaño; i < cantidadDeNiños; i++) {
+            niños[i - tamaño] = niños[i];
+        }
+        for (int i = cantidadDeNiños - tamaño; i < cantidadDeNiños; i++) {
+            niños[i] = null;
+        }
+    }
+
+    private Niño[] extraerGrupo(int tamaño) {
+        return Arrays.copyOf(niños, tamaño);
+    }
+
+    private boolean haySuficientesNinos() {
+        if (cantidadDeNiños < 5) {
+            System.out.println("No hay suficientes niños para iniciar el juego (Mínimo 5).\n");
+            return false;
+        }
+        return true;
+    }
+
+    private void mostrarGrupo(Niño[] grupo) {
+        for (Niño n : grupo) {
+            if (n != null) System.out.println("- " + n.nombre());
+            else System.out.println("niño nulo");
         }
         System.out.println();
     }
 
+    public String nombre() { return nombre; }
+    public int cantidadDeNiños() { return cantidadDeNiños; }
 }
